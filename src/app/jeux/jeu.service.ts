188db8cbee169323f7c0bd921a0c3528d738499d
martin_houziaux <martin_houziaux@ens.univ-artois.fr>
2021-03-31 21:23:22 +0200

    Version fonctionelle.

--- src/app/jeux/jeu.service.ts
+++ src/app/jeux/jeu.service.ts
@@ -1,42 +1,28 @@
-import { Injectable } from '@angular/core';
-<<<<<<< HEAD
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError, map, tap} from 'rxjs/operators';
+import {catchError, map, shareReplay, tap} from 'rxjs/operators';
 import {Jeu} from './jeu';
 import {Datas} from './jeux-data';
 
-=======
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import {Observable, pipe, throwError} from 'rxjs';
-import {catchError, map, shareReplay, tap} from 'rxjs/operators';
-import {Jeu} from './jeu';
 import {MessagesService} from '../messages/messages.service';
 import {Type} from '@angular/compiler';
 import {environment} from '../../environments/environment';
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
-<<<<<<< HEAD
 @Injectable({
   providedIn: 'root'
 })
-=======
-@Injectable({ providedIn: 'root' })
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
+@Injectable({providedIn: 'root'})
 export class JeuService {
   private readonly apiUrl = 'http://localhost:8000/api';
   jeux: Jeu[];
   map: Map<number, Jeu>;
 
-<<<<<<< HEAD
-  constructor(private http: HttpClient) {
-=======
   constructor(private http: HttpClient, private messagesService: MessagesService) {
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
   }
 
   getJeu(id: number): Observable<Jeu> {
@@ -47,17 +33,20 @@ export class JeuService {
         catchError(err => throwError(err))
       );
   }
+
   // @ts-ignore
-<<<<<<< HEAD
   getJeux(sort?: number): Observable<Jeu[]> {
-    if (sort === undefined) { const url = 'http://localhost:8000/api/jeux';
-                              return this.http.get<any>(url, httpOptions)
+    if (sort === undefined) {
+      const url = 'http://localhost:8000/api/jeux';
+      return this.http.get<any>(url, httpOptions)
         .pipe(
           map(res => res.data.item),
           tap(body => console.log(' **http** ', body))
-        ); }
-    if (sort > 0) { const url = 'http://localhost:8000/api/jeux?sort=nom';
-                    return this.http.get<any>(url, httpOptions)
+        );
+    }
+    if (sort > 0) {
+      const url = 'http://localhost:8000/api/jeux?sort=nom';
+      return this.http.get<any>(url, httpOptions)
         .pipe(
           map(res => res.data.item),
           tap(body => console.log(' **http** ', body)),
@@ -65,9 +54,10 @@ export class JeuService {
             console.log(err);
             return throwError('bug');
           }));
-         }
-    if (sort < 0) { const url = 'http://localhost:8000/api/jeux?sort=note';
-                    return this.http.get<any>(url, httpOptions)
+    }
+    if (sort < 0) {
+      const url = 'http://localhost:8000/api/jeux?sort=note';
+      return this.http.get<any>(url, httpOptions)
         .pipe(
           map(res => res.data.item),
           tap(body => console.log(' **http** ', body)),
@@ -97,17 +87,10 @@ export class JeuService {
     });
   }*/
 
-}
-=======
-  getJeux(): Observable<Type> {
-    return this.http.get<any>(environment.apiUrl + '/jeux', httpOptions)
-      .pipe(
-        map(rep => rep.data.item),
-        catchError(err => throwError(err))
-      );
-  }
 
-  // tslint:disable-next-line:max-line-length
+// @ts-ignore
+
+// tslint:disable-next-line:max-line-length
   addJeu(nom: string, description: string, regles: string, langue: string, url_media: string, age: number, poids: number, nombre_joueurs: number, categorie: string, duree: number, theme: string, editeur: string): Observable<Jeu> {
     // tslint:disable-next-line:max-line-length
     return this.http.post<any>(environment.apiUrl + '/jeux', {nom, description, theme, editeur, url_media, langue, age, poids, nombre_joueurs, duree, regles, categorie}, httpOptions)
@@ -123,6 +106,8 @@ export class JeuService {
           console.log(err);
           return throwError('bug');
         }));
+
   }
-  }
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
+}
+
+
--- src/app/jeux/jeux-details.component.ts
+++ src/app/jeux/jeux-details.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {JeuService} from './jeu.service';
-<<<<<<< HEAD
-=======
 import { Observable } from 'rxjs';
 import {Type} from '@angular/compiler';
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
 import {Jeu} from './jeu';
 
 // @ts-ignore
@@ -13,71 +10,54 @@ import {Jeu} from './jeu';
 @Component({
   selector: 'app-jeux-details',
   template: `
-<<<<<<< HEAD
     <div>
-        <div *ngIf="jeu.id == id">
-        <div class="p-d-flex p-flex-column">
-          <div class="p-text-center" style="font-size: xx-large; font-weight: bold">Jeu</div>
-          <div class="p-d-flex  p-flex p-jc-around">
-            <div style="font-weight: bold">{{jeu.nom}}</div>
-            <div style="font-weight: bold">{{jeu.description}}</div>
-            <div style="font-weight: bold">{{jeu.regles}}</div>
-            <div style="font-weight: bold">{{jeu.langue}}</div>
-            <div style="font-weight: bold">{{jeu.url_media}}</div>
-            <div style="font-weight: bold">{{jeu.age}}</div>
-            <div style="font-weight: bold">{{jeu.poids}}</div>
-            <div style="font-weight: bold">{{jeu.nombre_joueurs}}</div>
-            <div style="font-weight: bold">{{jeu.categorie}}</div>
-            <div style="font-weight: bold">{{jeu.duree}}</div>
-            <div style="font-weight: bold">{{jeu.note}}</div>
-          </div>
-        </div>
-      </div>
-      <p> Jeu sélectionnée : {{id}} {{jeu.nom}}</p>
+      <tr>
+        <th>#</th>
+        <th>Nom</th>
+        <th>Description</th>
+        <th>Thème</th>
+        <th>Editeur</th>
+        <th>URL Media</th>
+        <th>Langue</th>
+        <th>Âge</th>
+        <th>Poids</th>
+        <th>Nombre Joueurs</th>
+        <th>Durée</th>
+        <th>Catégorie</th>
+        <th>Règles</th>
+        <th>Note</th>
+      </tr>
+    </div>
+    <div>
+      <tr>
+        <td>{{jeux.nom}}</td>
+        <td>{{jeux.description}}</td>
+        <td>{{jeux.regles}}</td>
+        <td>{{jeux.langue}}</td>
+        <td>{{jeux.url_media}}</td>
+        <td>{{jeux.age}}</td>
+        <td>{{jeux.poids}}</td>
+        <td>{{jeux.nombre_joueurs}}</td>
+        <td>{{jeux.categorie}}</td>
+        <td>{{jeux.duree}}</td>
+      </tr>
     </div>
   `,
   styles: [
   ],
-=======
-        <div>
-          <tr>
-            <td>{{jeux.nom}}</td>
-            <td>{{jeux.description}}</td>
-            <td>{{jeux.regles}}</td>
-            <td>{{jeux.langue}}</td>
-            <td>{{jeux.url_media}}</td>
-            <td>{{jeux.age}}</td>
-            <td>{{jeux.poids}}</td>
-            <td>{{jeux.nombre_joueurs}}</td>
-            <td>{{jeux.categorie}}</td>
-            <td>{{jeux.duree}}</td>
-          </tr>
-        </div>
-    `,
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
+
 })
 
 export class JeuxDetailsComponent implements OnInit {
   id: number;
-<<<<<<< HEAD
-  jeu: Jeu;
-  constructor(public route: ActivatedRoute, public jeuxService: JeuService) { }
-=======
   jeu: Observable<Jeu>;
   jeux: Jeu;
   constructor(public route: ActivatedRoute, public jeuService: JeuService) { }
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.id = +this.route.snapshot.paramMap.get('id');
-<<<<<<< HEAD
-    this.jeu = this.jeuxService.getJeu(this.id);
-    console.log(this.jeu);
-    console.log('Id depense :  ', id);
-=======
     this.jeuService.getJeu(this.id).subscribe(jeux => this.jeux = jeux);
     console.log(this.jeux);
->>>>>>> ba78b4275ab39b0b02d51020c5b4e117e3295f0b
   }
 }
